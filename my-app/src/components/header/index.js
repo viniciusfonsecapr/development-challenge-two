3aa70dc1d4a01dfd77b884acfc272fb03caee8b9
Vinicius Gabriel Fonseca <vinicius.fonseca20093004@gmail.com>
2022-10-11 00:22:37 -0300

    Header atualizado

--- my-app/src/components/header/index.js
+++ my-app/src/components/header/index.js
@@ -5,27 +5,38 @@ import { Container } from './styles'
 import Logo from '../../assets/logo.png'
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link'
+import Button from '@mui/material/Button'
 
 function Header() {
 
     return (
         <Container>
-            <Box sx={{ display: 'flex', flexDirection: 'row'}}>
-                <img src={Logo} alt="logo" style={{ width: '140px', marginLeft: '50px' }}></img>
-                <Box sx={{width:'400px',margin:'20px 0px 0px 400px',display: 'flex', flexDirection: 'row',justifyContent:'space-around'}}>
-                    <Link href="#" underline="none" sx={{color:'white'}}>
+            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                <img src={Logo} alt="logo" style={{ width: '140px', marginLeft: '40px' }}></img>
+                <Box sx={{ width: '500px', marginTop: '20px', marginLeft:'20%', display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignContent:'center', }}>
+                    <Link href="#" underline="none" sx={{ color: 'black' }}>
                         HOME
                     </Link>
-                    <Link href="#" underline="none" sx={{color:'white'}}>
+                    <Link href="#" underline="none" sx={{ color: 'black' }}>
                         Cadastrar Pacientes
                     </Link>
-                    <Link href="#" underline="none" sx={{color:'white'}}>
+                    <Link href="#" underline="none" sx={{ color: 'black' }}>
                         About
                     </Link>
-                </Box>
-
+                    <Button sx={{
+                        marginTop: '-10px',
+                        width: '131px',
+                        height: '40px',
+                        background: '#0E20FF',
+                        color:'white',
+                        border: '1px solid rgba(0, 0, 0, 0.15)',
+                        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+                        borderRadius: '20px'}}>Github
+                    </Button>
             </Box>
-        </Container>
+
+        </Box>
+        </Container >
     );
 }
 
