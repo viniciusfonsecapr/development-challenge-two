38f424304c265ed0f62dca1a965a28d8047797a5
Vinicius Gabriel Fonseca <vinicius.fonseca20093004@gmail.com>
2022-10-18 02:32:15 -0300

    Ajustando botoes edit e delete

--- my-app/src/components/card-registration/index.js
+++ my-app/src/components/card-registration/index.js
@@ -11,7 +11,7 @@ import Button from '@mui/material/Button';
 
 const paddingItems = {
     paddingTop: '5px',
-    paddingBottom:'2px',
+    paddingBottom:'1px',
     fontFamily:'roboto'
 }
 
@@ -23,12 +23,12 @@ function CardRegistration({ users }) {
 
 
                  {users &&  users.map((user) => (
-                    <div key={user.id} style={{ width: '330px', padding: '25px', marginTop: '4%', marginLeft: '3%', backgroundColor: '#fff',boxShadow: '0px 40px 160px rgba(55, 76, 108, 0.24)' ,borderRadius: '12px' }}>
+                    <div key={user.id} style={{ width: '330px', height:'250px',padding: '25px', marginTop: '4%', marginLeft: '3%', backgroundColor: '#fff',boxShadow: '0px 40px 160px rgba(55, 76, 108, 0.24)' ,borderRadius: '12px' }}>
                         <Typography variant="h5" sx={{fontWeight:'bold'}} >{user.name}</Typography>
                         <Typography style={paddingItems}>Data de Nascimento: {user.date_birth}</Typography>
                         <Typography style={paddingItems}>Email: {user.email}</Typography>
                         <Typography style={paddingItems}>Endereço: {user.address}</Typography>
-                        <Box sx={{float:'right',mt:10}}>
+                        <Box sx={{display:'flex', flexDirectionmn:'row', justifyContent:'space-evenly',mt:1}}>
                            <Button sx={{mr:1}}>
                                <EditIcon/>
                             </Button>
